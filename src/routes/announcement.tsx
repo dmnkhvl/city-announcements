0a12a9c7e8bd7daf7d40a9cbb9a66b2b59e64a61
Dominik Havel <dev@dmnkhvl.eu>
2024-10-09 19:36:33 +0200

    fix: edit announcement

--- src/routes/announcement.tsx
+++ src/routes/announcement.tsx
@@ -62,82 +62,84 @@ export default function AnnouncementPage() {
   }
 
   return (
-    <div className="px-80 py-12 w-full">
-      <header className="flex items-center gap-x-2 mb-6">
-        <button onClick={() => navigate(-1)}>
-          <IoChevronBackOutline size={24} />
-        </button>
-        <h1 className="pageTitle">Edit Announcement</h1>
-      </header>
-      <form className="w-full max-w-[80%]">
-        <main className="flex flex-col gap-y-6">
-          <section className="flex flex-col gap-y-4">
-            <div>
-              <label className="formLabel" htmlFor="title">
-                Title
-              </label>
-              <input
-                type="text"
-                id="title"
-                name="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className="border border-gray-300 rounded-lg px-4 py-2 w-full"
-                placeholder="Announcement title"
-              />
-            </div>
-            <div>
-              <label className="formLabel" htmlFor="content">
-                Content
-              </label>
-              <textarea
-                id="content"
-                name="content"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-                className="border border-gray-300 rounded-lg px-4 py-2 w-full h-64"
-                placeholder="Announcement content"
-              />
-            </div>
-          </section>
-          <section>
-            <h2 className="pageSubTitle">Category</h2>
-            <p className="formLabel pb-2">
-              Select category so readers know what your announcement is about.
-            </p>
-            <Select
-              id="categories"
-              options={options}
-              isMulti
-              value={selectedCategories}
-              onChange={handleCategoryChange}
-              className="mb-4"
-              classNamePrefix="select"
-            />
-          </section>
-          <section>
-            <h2 className="pageSubTitle mb-4">Publication Date</h2>
-            <input
-              type="datetime-local"
-              id="publicationDate"
-              name="publicationDate"
-              value={publicationDate}
-              onChange={(e) => setPublicationDate(e.target.value)}
-              className="border border-gray-300 rounded-lg px-4 py-2 w-full"
-              placeholder="MM/DD/YYYY HH:mm"
-            />
-          </section>
-        </main>
-        <footer className="w-full justify-end flex pt-10">
-          <button
-            type="button"
-            className="px-4 py-2 rounded-full bg-action"
-            onClick={handlePublish}
-          >
-            <p className="font-bold">Publish</p>
+    <div className="py-12 w-full flex justify-center">
+      <div className="w-[800px] px-10">
+        <header className="flex items-center gap-x-2 mb-6">
+          <button onClick={() => navigate(-1)}>
+            <IoChevronBackOutline size={24} />
           </button>
-        </footer>
-      </form>
+          <h1 className="pageTitle">Edit Announcement</h1>
+        </header>
+        <form className="w-full max-w-[80%]">
+          <main className="flex flex-col gap-y-6">
+            <section className="flex flex-col gap-y-4">
+              <div>
+                <label className="formLabel" htmlFor="title">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+                  placeholder="Announcement title"
+                />
+              </div>
+              <div>
+                <label className="formLabel" htmlFor="content">
+                  Content
+                </label>
+                <textarea
+                  id="content"
+                  name="content"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                  className="border border-gray-300 rounded-lg px-4 py-2 w-full h-64"
+                  placeholder="Announcement content"
+                />
+              </div>
+            </section>
+            <section>
+              <h2 className="pageSubTitle">Category</h2>
+              <p className="formLabel pb-2">
+                Select category so readers know what your announcement is about.
+              </p>
+              <Select
+                id="categories"
+                options={options}
+                isMulti
+                value={selectedCategories}
+                onChange={handleCategoryChange}
+                className="mb-4"
+                classNamePrefix="select"
+              />
+            </section>
+            <section>
+              <h2 className="pageSubTitle mb-4">Publication Date</h2>
+              <input
+                type="datetime-local"
+                id="publicationDate"
+                name="publicationDate"
+                value={publicationDate}
+                onChange={(e) => setPublicationDate(e.target.value)}
+                className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+                placeholder="MM/DD/YYYY HH:mm"
+              />
+            </section>
+          </main>
+          <footer className="w-full justify-end flex pt-10">
+            <button
+              type="button"
+              className="px-4 py-2 rounded-full bg-action"
+              onClick={handlePublish}
+            >
+              <p className="font-bold">Publish</p>
+            </button>
+          </footer>
+        </form>
+      </div>
     </div>
   )
 }
